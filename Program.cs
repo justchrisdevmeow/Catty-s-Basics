f084c772d5bac13c8e6e2dc9f4ed0d3f7ca29042
notchwis <kodenothing@gmail.com>
2026-04-09 20:55:26 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ static class Program
         Raylib.InitWindow(800, 600, "Catty's Basics");
         Raylib.SetTargetFPS(60);
         
-        // Load cat image (put "catty.png" in the same folder as your .exe)
+        // Load cat image
         Texture2D catTexture = Raylib.LoadTexture("catty.png");
         
         Vector2 playerPos = new(400, 300);
@@ -27,18 +27,18 @@ static class Program
         {
             float dt = Raylib.GetFrameTime();
             
-            // Player movement (WASD)
-            if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) playerPos.Y -= 300 * dt;
-            if (Raylib.IsKeyDown(KeyboardKey.KEY_S)) playerPos.Y += 300 * dt;
-            if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) playerPos.X -= 300 * dt;
-            if (Raylib.IsKeyDown(KeyboardKey.KEY_D)) playerPos.X += 300 * dt;
+            // Player movement
+            if (Raylib.IsKeyDown(KeyboardKey.W)) playerPos.Y -= 300 * dt;
+            if (Raylib.IsKeyDown(KeyboardKey.S)) playerPos.Y += 300 * dt;
+            if (Raylib.IsKeyDown(KeyboardKey.A)) playerPos.X -= 300 * dt;
+            if (Raylib.IsKeyDown(KeyboardKey.D)) playerPos.X += 300 * dt;
             
-            // Catty AI: chase player
+            // Catty AI
             Vector2 dir = playerPos - catPos;
             if (dir.Length() > 0)
                 catPos += dir / dir.Length() * 200 * catSpeed * dt;
             
-            // Collision with Catty = game over
+            // Collision
             if (Vector2.Distance(playerPos, catPos) < 30)
             {
                 Raylib.CloseWindow();
@@ -57,35 +57,35 @@ static class Program
             
             // Drawing
             Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.DARKGREEN);
+            Raylib.ClearBackground(Color.Green);
             
             // Draw notebooks
             foreach (var n in notebooks)
-                Raylib.DrawRectangle((int)n.X - 10, (int)n.Y - 10, 20, 20, Color.GOLD);
+                Raylib.DrawRectangle((int)n.X - 10, (int)n.Y - 10, 20, 20, Color.Yellow);
             
-            // Draw Catty with your PNG
-            Raylib.DrawTexture(catTexture, (int)catPos.X - 25, (int)catPos.Y - 25, Color.WHITE);
+            // Draw Catty
+            Raylib.DrawTexture(catTexture, (int)catPos.X - 25, (int)catPos.Y - 25, Color.White);
             
             // Draw player
-            Raylib.DrawCircleV(playerPos, 15, Color.BLUE);
+            Raylib.DrawCircleV(playerPos, 15, Color.Blue);
             
             // UI
-            Raylib.DrawText($"Wrong: {wrongAnswers}", 10, 10, 20, Color.WHITE);
-            Raylib.DrawText($"Catty Speed: x{catSpeed:F1}", 10, 35, 20, Color.WHITE);
+            Raylib.DrawText($"Wrong: {wrongAnswers}", 10, 10, 20, Color.White);
+            Raylib.DrawText($"Catty Speed: x{catSpeed:F1}", 10, 35, 20, Color.White);
             
-            // Math problem popup
+            // Math problem
             if (showMath)
             {
-                Raylib.DrawRectangle(200, 200, 400, 150, Color.DARKGRAY);
-                Raylib.DrawText("2 + 2 = ?", 220, 230, 30, Color.WHITE);
-                Raylib.DrawText("Press Y for 4, N for wrong", 220, 280, 20, Color.LIGHTGRAY);
+                Raylib.DrawRectangle(200, 200, 400, 150, Color.Gray);
+                Raylib.DrawText("2 + 2 = ?", 220, 230, 30, Color.White);
+                Raylib.DrawText("Press Y for 4, N for wrong", 220, 280, 20, Color.LightGray);
                 
-                if (Raylib.IsKeyPressed(KeyboardKey.KEY_Y))
+                if (Raylib.IsKeyPressed(KeyboardKey.Y))
                 {
                     showMath = false;
                     notebooks.RemoveAt(currentNotebook);
                 }
-                else if (Raylib.IsKeyPressed(KeyboardKey.KEY_N))
+                else if (Raylib.IsKeyPressed(KeyboardKey.N))
                 {
                     wrongAnswers++;
                     catSpeed = 1f + wrongAnswers * 0.3f;
